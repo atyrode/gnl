f9cb7273d3b8b8e8c018ab5a2d502aff7223327f
Alex TYRODE <atyrode@e1r6p1.42.fr>
2017-09-22 21:03:48 +0200

    gnl yay

--- get_next_line.c
+++ get_next_line.c
@@ -5,80 +5,101 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: atyrode <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/18 13:19:20 by atyrode           #+#    #+#             */
-/*   Updated: 2017/09/18 13:41:03 by atyrode          ###   ########.fr       */
+/*   Created: 2017/09/22 20:38:32 by atyrode           #+#    #+#             */
+/*   Updated: 2017/09/22 20:55:00 by atyrode          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int				get_next_line(const int fd, char **line)
+int		ft_strfindchr(char *str, char c)
 {
-	char		*buff;
-	static char	*str = NULL;
-	int			ret;
-	char		**tab_ret;
-	char		*tempstr;
-	int			yes;
-	int			test;
-	int			fl;
+	int		i;
 
-	test = 0;
-	yes = 1;
-	fl = 0;
-	ft_strclr(*line);
-	if (fd < 0)
-		return (-1);
-	buff = malloc(sizeof(char) * BUFF_SIZE + 1);
-	if (str == NULL)
+	if (c == '\0')
+		return (ft_strlen(str));
+	i = 0;
+	while (str[i] != '\0')
 	{
-		str = malloc(sizeof(char) * BUFF_SIZE + 1);
-		*line = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
-		fl = 1;
+		if (str[i] == c)
+			return (i);
+		else
+			i++;
+	}
+	return (-1);
+}
+
+int		ft_transfer_else(char *src, char **dest, int pos, char *tmp)
+{
+	int		i;
+
+	i = 0;
+	src[pos] = '\0';
+	*dest = ft_strjoin(*dest, src);
+	free(tmp);
+	pos++;
+	while (src[pos] != '\0')
+	{
+		src[i] = src[pos];
+		i++;
+		pos++;
+	}
+	src[i] = '\0';
+	return (1);
+}
+
+int		ft_transfer(char *src, char **dest)
+{
+	int		pos;
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	pos = ft_strfindchr(src, '\n');
+	tmp = *dest;
+	if (pos == -1)
+	{
+		*dest = ft_strjoin(*dest, src);
+		free(tmp);
+		src[0] = '\0';
+		return (0);
 	}
 	else
-	{
-		if (str[0] == '\n' && str[1] == '\n')
-			str++;
-		if (!(ft_strchr(str + 1, '\n')))
-			ft_strcat(*line, str + 1);
-		else if (str[0] == '\n' && str[1] != '\0' && str[1] != '\n')
-		{
-			tab_ret = ft_strsplit(str + 1, '\n');
-			str = ft_strchr(str + 1, '\n');
-			ft_strcat(*line, tab_ret[0]);
-			yes = 0;
-		}
-	}
-	tempstr = malloc(sizeof(char) * BUFF_SIZE + 1);
-	while ((ret = read(fd, buff, BUFF_SIZE)) && (!(ft_strchr(buff, '\n'))))
-	{
-		if (ret < 0)
-			return (-1);
-		buff[ret] = '\0';
-		ft_strcat(*line, buff);
-	}
-	if (ret == 0 && str[0] == '\0' && fl == 0)
-	{
-		free(str);
-		return (0);
-	}
-	ft_strcat(str, buff);
-	if (ret == 0 && ((str[0] == '\0') || (str[0] == '\n' && str[1] == '\0')))
-		return (0);
-	else if (yes == 1 && (tempstr = ft_strchr(buff, '\n')))
-	{
-		str = ft_strdup(tempstr);
-		tab_ret = ft_strsplit(buff, '\n');
-		if (buff[0] != '\n')
-			ft_strcat(*line, tab_ret[0]);
+		return (ft_transfer_else(src, dest, pos, tmp));
+}
+
+int		normidead(char *buffer, int read_ret, char **line, const int fd)
+{
+	if (ft_transfer(buffer, line))
 		return (1);
-	}
-	free(buff);
-	if ((!(ft_strchr(str + 1, '\n'))) && (ft_strchr(str, '\0')))
+	while ((read_ret = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
-		ft_strclr(str);
-		return (1);
+		buffer[read_ret] = '\0';
+		if (ft_transfer(buffer, line))
+			return (1);
 	}
-	return (0);
+	if (read_ret == 0)
+		return (ft_strlen(*line) == 0 ? 0 : 1);
+	free(*line);
+	*line = NULL;
+	return (-1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char		*buffer;
+	static int		current_fd = -1;
+	int				read_ret;
+
+	read_ret = 0;
+	if (fd < 0 || !line)
+		return (-1);
+	if (!buffer && (buffer = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1)))
+			== NULL)
+		return (-1);
+	if (fd != current_fd)
+		buffer[0] = '\0';
+	current_fd = fd;
+	*line = ft_strnew(0);
+	return (normidead(buffer, read_ret, line, fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,21 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: atyrode <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/12 10:22:00 by atyrode           #+#    #+#             */
-/*   Updated: 2017/09/17 20:36:11 by atyrode          ###   ########.fr       */
+/*   Created: 2017/09/22 20:47:38 by atyrode           #+#    #+#             */
+/*   Updated: 2017/09/22 21:02:35 by atyrode          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <stdlib.h>
-# include <unistd.h>
 # include <fcntl.h>
-# include <stdio.h>
-# include "libft/libft.h"
+# include <stdlib.h>
+# include "./libft/includes/libft.h"
 
-# define BUFF_SIZE 4
-int				get_next_line(int fd, char **line);
+# define BUFF_SIZE 5
+
+int	get_next_line(const int fd, char **line);
 
 #endif
